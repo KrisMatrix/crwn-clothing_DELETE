e8e9f580a900eb625a86bc5ced4b30737acd4ba2
Krishna Karnamadakala <krishna@hindulit.com>
2021-11-13 21:08:16 -0700

    Modified project to include some react-router-dom.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>
+      <Route exact path='/' component={ HomePage } />
+      <Route path='/hats' component={ HatsPage } />
+      </Switch>
     </div>
   );
 }
+
+/* 
+ * You really only need Route with exact path for simple routing for SPA.
+ * However, if you use Route but don't use exact path and instead use 
+ * only path, then it will render all the components in a greedy manner.
+ * You can use Switch to render the first 'path' (not exact path) instead.
+ *
+ */
 
 export default App;
