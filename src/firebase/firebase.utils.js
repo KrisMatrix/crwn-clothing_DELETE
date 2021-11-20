9ac68273d42416cbca2cefe067011cc45a3be7b2
Krishna Karnamadakala <krishna@hindulit.com>
2021-11-20 15:31:58 -0700

    Added firebase for signin and sign up complete. There is a bug here though with onSnapshot in App.js

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,30 @@ const config = {
   measurementId: "G-CGD5TXRF2R"
 };
 
+export const createUserProfileDocument = async(userAuth, additionalData) => {
+  if (!userAuth) {
+    return
+  }
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get(); //get object for user from firestore db
+                                        //If no such user, returns emtpy object
+  if (!snapShot.exists) { //if user does not exists in firestore db, add user.
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
